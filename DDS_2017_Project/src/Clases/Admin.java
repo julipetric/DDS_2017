f4be4e8e0950592ad5749e67065fbf56154b6ed4
Santiago Roa <santiagot.roa@gmail.com>
2017-10-10 09:07:17 -0300

    10 octubre

--- DDS_2017_Project/src/Clases/Admin.java
+++ DDS_2017_Project/src/Clases/Admin.java
@@ -1,13 +1,4 @@
-<<<<<<< HEAD:DDS_2017_Project/src/dds_2017/DDS_2017.java
-package dds_2017;
-=======
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Clases;
->>>>>>> master:DDS_2017_Project/src/Clases/Admin.java
 
 /**
  *
@@ -15,14 +6,8 @@ package Clases;
  */
 public class Admin extends Usuario{
 
-<<<<<<< HEAD:DDS_2017_Project/src/dds_2017/DDS_2017.java
-   
-    public static void main(String[] args) {
-        // TODO code application logic here
-        //añadidas librerias  santo
-=======
     public Admin() {
->>>>>>> master:DDS_2017_Project/src/Clases/Admin.java
+
     }
     
 }
