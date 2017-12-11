1be2d60176ff24d0bd24b87ab7fca18f20230a32
Santiago Roa <santiagot.roa@gmail.com>
2017-12-11 13:03:47 -0300

    buscar bedel beta V1.2

--- DDS_2017_Project/src/DAO/UsuarioDAO.java
+++ DDS_2017_Project/src/DAO/UsuarioDAO.java
@@ -6,20 +6,26 @@
 package DAO;
 
 import Clases.Turno;
-import Interfaces.TODO_OK;
+import Clases.Usuario;
 import bd.dto.HibernateUtil;
 import bd.model.Bedel;
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
+import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 import org.hibernate.Criteria;
 import org.hibernate.Query;
+import org.hibernate.SQLQuery;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
-import org.hibernate.cfg.Configuration;
+import org.hibernate.criterion.Conjunction;
+import org.hibernate.criterion.MatchMode;
+import static org.hibernate.criterion.Projections.id;
 import org.hibernate.criterion.Restrictions;
 
 /**
@@ -57,20 +63,43 @@ public class UsuarioDAO {
         tx.commit();
         session.close();
     }
-    
-    public List read(String apellido, Turno turno){
-        
-        SessionFactory sesion = HibernateUtil.getSessionFactory();
-        Session session = sesion.openSession();
-        
-        String hql = "FROM Bedel";
-        Query query = session.createQuery(hql);
-        List bedeles = query.list();
-        
-        System.out.println(bedeles);
-        
 
-        return bedeles;
+    public List read(String apellido, Turno turno) {
+        
+       SessionFactory sessionFactory = HibernateUtil.getSessionFactory();
+        Session session = sessionFactory.getCurrentSession();   
+        Transaction tx = session.beginTransaction();
+        Criteria crit = session.createCriteria(Bedel.class);
+        Conjunction conj = Restrictions.conjunction();
+        ArrayList <Bedel> resultado;
+        
+        conj.add(Restrictions.ilike("apellido", apellido, MatchMode.ANYWHERE));
+        crit.add(conj);
+        resultado = (ArrayList<Bedel>)crit.list();
+        
+        for(int i=0; i<=resultado.size(); i++){
+            System.out.print(resultado.get(i).getNombre()); 
+        }
+        
+        session.getTransaction().commit();
+        
+        /*SQLQuery query = session.createSQLQuery("select nombre, apellido, nombreUsuario, password, turno from bedel").addEntity(Bedel.class);
+        List bedeles = query.list();*/
+        
+  
+        
+         /* for (Iterator iterator = bedeles.iterator(); iterator.hasNext();){
+            Bedel employee = (Bedel) iterator.next(); 
+            System.out.print("First Name: " + employee.getNombre()); 
+            System.out.print("  Last Name: " + employee.getApellido()); 
+         }
+         
+
+        //System.out.println(bedeles);
+        tx.commit();
+        session.close();
+        */
+        return null;
     }
 
 }
